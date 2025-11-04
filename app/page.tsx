7d5e5f05a05494c4b7ac1bfc9d1e65846c8df662
Long <long.nguyen@swagsoft.com>
2025-11-04 09:55:31 +0700

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+const page = () => {
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br /> Event you Can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
+    </section>
+  );
+};
+
+export default page;
